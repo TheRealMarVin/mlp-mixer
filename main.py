c10a68413b2dbb6a240c923aa53be4a87101016f
therealmarvin <nakedtoothbrush@hotmail.com>
2021-06-29 21:32:16 -0400

    basic main function

---
+++ main.py
@@ -0,0 +1,73 @@
+import torch
+import torch.nn as nn
+
+import os
+from os import path
+
+import torchvision
+from matplotlib.pyplot import imshow
+from torch import optim
+
+from torch.utils.data import SubsetRandomSampler
+from torchvision import transforms
+
+from mlp_mixer.mlp_mixer import MlpMixer
+from train_eval.eval import evaluate
+from train_eval.history import History
+from train_eval.training import train
+
+def get_mnist_sets(transform):
+    train_set = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    test_set = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+    return train_set, test_set
+
+def get_cifar10_sets(transform):
+    train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+
+    return train_set, test_set
+
+def run_experiment():
+    n_epochs = 200
+    batch_size = 32
+    learning_rate = 0.00005
+
+    model_name = "mlp_mixer"
+
+    out_folder = "models/{}".format(model_name)
+    if not path.exists(out_folder):
+        os.makedirs(out_folder)
+
+    save_file = "{}/best.model".format(out_folder)
+
+    transform = transforms.Compose([transforms.ToTensor()])
+
+    train_set, test_set = get_mnist_sets(transform)
+    # train_set, test_set = get_cifar10_sets(transform)
+    # train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, num_workers=4,
+    #                                            shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=4)
+
+    model = MlpMixer(input_size=14,
+                     nb_blocks=1,
+                     nb_channels=1,
+                     out_size=10,
+                     hidden_size=196,
+                     dropout=0.25)
+    if torch.cuda.is_available():
+        model = model.cuda()
+
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    history = History()
+    train(model, train_set, optimizer, criterion, None, batch_size, n_epochs, True, history, save_file, early_stop=None,
+          true_index=1)
+    print('Finished Training')
+
+    _, _, valid_loss = evaluate(model, test_loader, criterion)
+    print("Done")
+
+if __name__ == '__main__':
+    run_experiment()

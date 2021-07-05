0010f0bc7db08f23a7c1e03d8d87943f9507f7a4
therealmarvin <nakedtoothbrush@hotmail.com>
2021-07-04 20:39:19 -0400

    create non square patch size. Let's see if this is a good thing or not

--- mlp_mixer/mixer_block.py
+++ mlp_mixer/mixer_block.py
@@ -9,13 +9,14 @@ from mlp_mixer.token_mixer import TokenMixer
 class MixerBlock(nn.Module):
     def __init__(self, image_input_size, nb_channels, patch_size, hidden_size, dropout):
         super(MixerBlock, self).__init__()
-        self.pre_norm_layer = nn.LayerNorm(int(patch_size * patch_size))
-        self.post_norm_layer = nn.LayerNorm(int(patch_size * patch_size))
-        self.token_mixer = TokenMixer(int((image_input_size * image_input_size) / (patch_size * patch_size)),
+        patch_area = int(patch_size[0] * patch_size[1])
+        self.pre_norm_layer = nn.LayerNorm(patch_area)
+        self.post_norm_layer = nn.LayerNorm(patch_area)
+        self.token_mixer = TokenMixer(int((image_input_size * image_input_size) / patch_area),
                                       hidden_size,
                                       nb_channels,
                                       dropout=dropout)
-        self.channel_mixer = ChannelMixer(patch_size * patch_size * nb_channels,
+        self.channel_mixer = ChannelMixer(patch_area * nb_channels,
                                           hidden_size,
                                           nb_channels,
                                           dropout=dropout)
--- mlp_mixer/mlp_mixer.py
+++ mlp_mixer/mlp_mixer.py
@@ -8,9 +8,13 @@ from mlp_mixer.mixer_block import MixerBlock
 class MlpMixer(nn.Module):
     def __init__(self, image_input_size, nb_channels, patch_size, nb_blocks, out_size, hidden_size, dropout):
         super(MlpMixer, self).__init__()
-        self.patch_size = patch_size
-        self.final_norm_layer = nn.LayerNorm(int((patch_size * patch_size)))
-        self.fc = nn.Linear(patch_size * patch_size * nb_channels, out_size)
+        if type(patch_size) is tuple:
+            self.patch_size = patch_size
+        else:
+            self.patch_size = (patch_size, patch_size)
+
+        self.final_norm_layer = nn.LayerNorm(int(self.patch_size[0] * self.patch_size[1]))
+        self.fc = nn.Linear(self.patch_size[0] * self.patch_size[1] * nb_channels, out_size)
 
         self.mixers = nn.ModuleList(
             [MixerBlock(image_input_size, nb_channels, patch_size, hidden_size, dropout) for _ in range(nb_blocks)]
@@ -31,16 +35,16 @@ class MlpMixer(nn.Module):
 
     def _create_patch(self, x):
         shape = x.shape
-        patch_count = (shape[-1] * shape[-2]) / (self.patch_size * self.patch_size)
+        patch_count = (shape[-1] * shape[-2]) / (self.patch_size[0] * self.patch_size[1])
 
         # unfold channels
         x = x.data.unfold(dimension=1, size=shape[1], step=shape[1])
         # unfold width
-        x = x.data.unfold(dimension=2, size=self.patch_size, step=self.patch_size)
+        x = x.data.unfold(dimension=2, size=self.patch_size[0], step=self.patch_size[0])
         # unfold height
-        x = x.data.unfold(dimension=3, size=self.patch_size, step=self.patch_size)
+        x = x.data.unfold(dimension=3, size=self.patch_size[1], step=self.patch_size[1])
 
         # reshape to [batch_size, patch_count, patch_size*patch_size]
-        x = x.reshape(shape[0], int(patch_count), shape[1] * self.patch_size * self.patch_size)
+        x = x.reshape(shape[0], int(patch_count), shape[1] * self.patch_size[0] * self.patch_size[1])
 
         return x

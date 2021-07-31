48881307139b01fb72fa3c08330193077f9eb888
TheRealMarVin <nakedtoothbrush@hotmail.com>
2021-07-31 06:53:38 -0400

    rework the patch size

--- mlp_mixer/mixer_block.py
+++ mlp_mixer/mixer_block.py
@@ -9,14 +9,16 @@ from mlp_mixer.token_mixer import TokenMixer
 class MixerBlock(nn.Module):
     def __init__(self, image_input_size, nb_channels, patch_size, hidden_size, dropout):
         super(MixerBlock, self).__init__()
+
+        nb_patch = int((image_input_size[0] * image_input_size[1] * nb_channels) / (patch_size[0] * patch_size[1]))
         patch_area = int(patch_size[0] * patch_size[1] * nb_channels)
-        self.pre_norm_layer = nn.LayerNorm(patch_area)
-        self.post_norm_layer = nn.LayerNorm(patch_area)
-        self.token_mixer = TokenMixer(int((image_input_size[0] * image_input_size[1]) / int(patch_size[0] * patch_size[1])),
-                                      hidden_size,
+        self.pre_norm_layer = nn.LayerNorm(hidden_size)
+        self.post_norm_layer = nn.LayerNorm(hidden_size)
+        self.token_mixer = TokenMixer(nb_patch,
+                                      hidden_size * 4,
                                       dropout=dropout)
-        self.channel_mixer = ChannelMixer(patch_area,
-                                          hidden_size,
+        self.channel_mixer = ChannelMixer(hidden_size,
+                                          hidden_size * 4,
                                           dropout=dropout)
 
     def forward(self, x):

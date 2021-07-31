07aa33998836f999d397aac5096cf7635e8b3a12
TheRealMarVin <nakedtoothbrush@hotmail.com>
2021-07-31 06:57:12 -0400

    add a fully connected right after the patch to transform them on hidden size

--- mlp_mixer/mlp_mixer.py
+++ mlp_mixer/mlp_mixer.py
@@ -11,8 +11,9 @@ class MlpMixer(nn.Module):
         self.patch_size = self._make_tuple(patch_size)
         self.image_input_size = self._make_tuple(image_input_size)
 
-        self.final_norm_layer = nn.LayerNorm(int(self.patch_size[0] * self.patch_size[1] * nb_channels))
-        self.fc = nn.Linear(self.patch_size[0] * self.patch_size[1] * nb_channels, out_size)
+        self.final_norm_layer = nn.LayerNorm(hidden_size)
+        self.patch_fc = nn.Linear(int(self.patch_size[0] * self.patch_size[1]), hidden_size)
+        self.fc = nn.Linear(hidden_size, out_size)
 
         self.mixers = nn.ModuleList(
             [MixerBlock(self.image_input_size, nb_channels, self.patch_size, hidden_size, dropout) for _ in range(nb_blocks)]
@@ -28,6 +29,7 @@ class MlpMixer(nn.Module):
 
     def forward(self, x):
         x = self._create_patch(x)
+        x = self.patch_fc(x)
 
         for curr_layer in self.mixers:
             x = curr_layer(x)
@@ -41,7 +43,7 @@ class MlpMixer(nn.Module):
 
     def _create_patch(self, x):
         shape = x.shape
-        patch_count = (shape[-1] * shape[-2]) / (self.patch_size[0] * self.patch_size[1])
+        patch_count = (shape[-1] * shape[-2]  * shape[-3]) / (self.patch_size[0] * self.patch_size[1])
 
         # unfold channels
         x = x.data.unfold(dimension=1, size=shape[1], step=shape[1])
@@ -51,6 +53,6 @@ class MlpMixer(nn.Module):
         x = x.data.unfold(dimension=3, size=self.patch_size[1], step=self.patch_size[1])
 
         # reshape to [batch_size, patch_count, patch_size*patch_size]
-        x = x.reshape(shape[0], int(patch_count), shape[1] * self.patch_size[0] * self.patch_size[1])
+        x = x.reshape(shape[0], int(patch_count), self.patch_size[0] * self.patch_size[1])
 
         return x
